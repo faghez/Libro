f57f8710b166876db7d30fd51a5fefd42a88041c
MarcosPC <MarcosPC@DESKTOP-TU8COEC>
2020-11-24 19:32:37 +0100

    Añadido capítulo 3

---
+++ Libro/src/capitulos/Capitulo3.java
@@ -0,0 +1,13 @@
+package capitulos;
+
+public class Capitulo3
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Git permite la creación de ramas lo que permite tener distintas versiones "
+            + "del mismo proyecto y trabajar de manera simultánea en ellas");
+
+    }
+
+}
