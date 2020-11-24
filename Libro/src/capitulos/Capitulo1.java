0399f1ac5afffe7ecbba978b95566b06a012fc22
MarcosPC <MarcosPC@DESKTOP-TU8COEC>
2020-11-24 19:27:44 +0100

    Añadido capítulo 1

---
+++ Libro/src/capitulos/Capitulo1.java
@@ -0,0 +1,11 @@
+package capitulos;
+
+public class Capitulo1
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Git es un sistema de control de versiones ideado por Linus Torvalds");
+    }
+
+}
