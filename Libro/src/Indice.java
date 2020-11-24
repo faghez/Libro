316be496b44ab8cedef21aef8b987ec9c572796a
MarcosPC <MarcosPC@DESKTOP-TU8COEC>
2020-11-24 19:19:58 +0100

    Añadido capítulo 3 sobre gestión de ramas

---
+++ Libro/src/Indice.java
@@ -0,0 +1,13 @@
+
+public class Indice
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Capítulo 1: Introducción a Git");
+        System.out.println("Capítulo 2: Flujo de trabajo básico");
+        System.out.println("Capítulo 3: Gestión de ramas");
+        System.out.println("Capítulo 4: Repositorios remotos");
+    }
+
+}
