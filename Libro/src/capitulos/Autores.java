cf7e71de1ad359495e7ad07b9fa88a553e32aa3b
MarcosPC <MarcosPC@DESKTOP-TU8COEC>
2020-11-25 11:10:43 +0100

    Añadido autor

---
+++ Libro/src/capitulos/Autores.java
@@ -0,0 +1,11 @@
+package capitulos;
+
+public class Autores
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Fátima Gutiérrez Hdez. (fatyguti@hotmail.com)");
+    }
+
+}
