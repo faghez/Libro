b46d9d8ccf5e9e686ebf675dd4142dc014f71b11
MarcosPC <MarcosPC@DESKTOP-TU8COEC>
2020-11-25 10:42:12 +0100

    Añadida nueva referencia bibliográfica

--- Libro/src/capitulos/Bibliografia.java
+++ Libro/src/capitulos/Bibliografia.java
@@ -5,7 +5,8 @@ public class Bibliografia
 
     public static void main(String[] args)
     {
-        System.out.println("Chacon, S. and Straun, B. Pro Git. Apress.");
+        System.out.println("Scott Chacon and Ben Straub. Pro Git. Apress.");
+        System.out.println("Ryan Hodson. Ry's Git Tutorial. Smashwords (2014)");
 
     }
 
