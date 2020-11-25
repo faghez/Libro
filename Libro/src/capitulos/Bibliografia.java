51358d28f905e55eeb7b6f91d0916fc0a76de739
MarcosPC <MarcosPC@DESKTOP-TU8COEC>
2020-11-25 10:46:06 +0100

    Añadida nueva referencia bibliográfica

--- Libro/src/capitulos/Bibliografia.java
+++ Libro/src/capitulos/Bibliografia.java
@@ -5,7 +5,8 @@ public class Bibliografia
 
     public static void main(String[] args)
     {
-        System.out.println("Chacon, S. and Straun, B. Pro Git. Apress.");
+        System.out.println("Chacon, S. and Straub, B. Pro Git. Apress.");
+        System.out.println("Loeliger, J. and McCullough, M. Version control with Git. O'Reilly");
 
     }
 
